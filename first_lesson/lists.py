ef67463a1c0f18ed4354fbfeef1c7134d80953d5
Rafael Medeiros Vieira <soluxya@gmail.com>
2018-12-02 19:51:56 -0200

    Finishing the list exercise

---
+++ first_lesson/lists.py
@@ -0,0 +1,23 @@
+mylist = []
+mylist.append(0)
+mylist.append(1)
+mylist.append(2)
+print(mylist[0])    # prints 1
+print(mylist[1])    # prints 2
+print(mylist[2])    # prints 3
+print(' this is the list: ')
+for x in mylist:
+    print(x)
+mylist = [1, 2, 3]
+print(mylist[2])
+n = 2, 3, 4
+s = "Hello", "World"
+numbers = []
+numbers.append(n)   # adding n to the list
+strings = []
+strings.append(s)   # adding s to the list
+names = ["John", "Eric", "Jessica"]
+second_name = ["Jake", "Logan", "Erika"]
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
