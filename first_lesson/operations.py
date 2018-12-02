0f9437f0664ce20a99519e260f400502dc23c801
Rafael Medeiros Vieira <soluxya@gmail.com>
2018-12-02 20:43:59 -0200

    finishing operands and ex2

---
+++ first_lesson/operations.py
@@ -0,0 +1,22 @@
+op = 54
+num = op * 3 + 3 +5 - 17 / 13 * 31.71
+print(num)
+
+num = 10 % 3
+print(num)
+print(op)
+
+s = 6 ** 8
+print(s)
+
+hello = "hello " * 10 + " " + "world " * 10
+print(hello)
+
+even_numbers = [2, 4, 6, 8]
+odd_numbers = [1, 3, 5, 7]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
+
+print([1, 2, 3] * 3)
+
+print(["number ", "is " "wrong %d" % num] * 2)
