200a944e9d28b4ebc0f0b948debd208cd9f2c65f
Rafael Medeiros Vieira <soluxya@gmail.com>
2018-12-02 22:13:36 -0200

    finishing the first lecture

---
+++ first_lesson/formatting.py
@@ -0,0 +1,18 @@
+name = "My name is a secret..."
+print("You, fiend, What has thou be called you bastard!!   %s  " % name)
+
+
+name = "Jonh Oliver del Grace"
+age = 20
+print("%s has %d of age, there is no way that he will be elected..." % (name, age))
+
+mylist = [13, 20, 355, 1313213, 20232, 1713231]
+print("%s And my list of numers for the election of 2790: %s" % (name, mylist))
+
+number = 13.450454545
+print("This float has only three digits after the point, see:  %.3f " % number)
+
+data = ("John", "Doe", 53.44)
+format_string = "Hello %s %s. Your balance is $%.2f."
+
+print(format_string % data)
